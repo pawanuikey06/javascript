73f39e43bb2c177c0778d02b908e0f0986abd8e3
pawanuikey06 <pawanuikey690@gmail.com>
2024-10-25 18:01:49 +0530

    Comparision

--- conversion&Operations/index.js
+++ conversion&Operations/index.js
@@ -6,4 +6,14 @@ console.log(typeof num);
 console.log(num)
 
 
+//  OPERATIONS & cOmperisions
 
+let value =3
+let ngvl =-value;
+
+console.log("2">3)
+console.log("02">1)
+console.log(null >0)
+console.log(null <0)
+console.log(null ==0)
+console.log(null >=0)
