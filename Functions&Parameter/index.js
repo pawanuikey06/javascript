31440331b56eb97daeb6282295fe00c61ae5dcbd
pawanuikey06 <pawanuikey690@gmail.com>
2024-10-27 13:03:11 +0530

    functions

---
+++ Functions&Parameter/index.js
@@ -0,0 +1,25 @@
+function calculateCartPrice(val,val2,...num1){
+    return num1
+}
+
+
+// console.log(calculateCartPrice(200,300,400))
+
+const user={
+    name:'pawan',
+    price:199
+
+}
+
+function handleObject(anyObject){
+    console.log(`UserName Is ${anyObject.name} and Price is ${anyObject.price}`)
+}
+
+handleObject(user)
+handleObject({
+    name :'sammy',
+    price:242141134
+})
+
+// Passing Array Inside the Function
+
