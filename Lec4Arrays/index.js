ae4abb4940e78f9d60dfb5517be35df1de37bfb8
pawanuikey06 <pawanuikey690@gmail.com>
2024-10-27 09:00:52 +0530

    new methods

--- Lec4Arrays/index.js
+++ Lec4Arrays/index.js
@@ -22,3 +22,30 @@ console.log(arr.includes(2423))
 console.log(arr.indexOf(13))
 console.log(arr.splice(1,3,55,66,77,88,88));
 console.log(arr.join())
+
+// Spread Operator
+
+let a =[1,2,3,4,5,6,7];
+let b =[8,9,0,33,44,55,666,7777]
+
+let c =[...a ,...b];
+console.log(c)
+b[3]=13233423;
+console.log(c)
+console.log(b)
+
+//  flatten Array 
+
+
+// Coverting into Array 
+// Using From Method
+
+let name="pawan"
+let temp =Array.from(name)
+console.log(temp)
+temp[3] ="cgd"
+console.log(temp)
+console.log(name)
+
+
+console.log(Array.from({name :"pawan"}))
