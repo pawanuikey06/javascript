59e41aacbe2d15a7ec2384a1b21dcd413798ebe8
pawanuikey06 <pawanuikey690@gmail.com>
2024-11-06 18:08:34 +0530

    Event Propogation

--- EVENTS/script.js
+++ EVENTS/script.js
@@ -2,7 +2,7 @@
 //     alert('OWL CLICKED')
 // }
 
-
+// best Approach
 document.getElementById('owl').addEventListener('click',(event)=>{
 
     console.log(event)
@@ -14,3 +14,38 @@ document.getElementById('owl').addEventListener('click',(event)=>{
 // ClientX,ClientY ,screenX,screenY ,altKEy ,ctrlKey,shiftkey,keyCode
 
 // EVENT PROPOGATION
+document.getElementById('images').addEventListener('click',(event)=>{
+
+    console.log("Clicked Inside The UL")
+
+})
+
+document.getElementById('owl').addEventListener('click',(event)=>{
+
+    console.log('Owl Clicked')
+    event.stopPropagation()
+
+})
+
+document.getElementById('google').addEventListener('click',(e)=>{
+    e.preventDefault()
+    console.log('google Clicked');
+    
+})
+
+
+
+document.querySelector('#images').addEventListener('click',(event)=>{
+    console.log(event);
+    console.log(event.target)
+    console.log(event.target.parentNode)
+    console.log(event.target.tagName)
+    if(event.target.tagName==='IMG'){
+        console.log(event.target.id)
+        let removeIt =event.target.parentNode
+    removeIt.remove()
+
+    }
+    // removeIt.parentNode.removeChild(removeIt)
+
+},false)
