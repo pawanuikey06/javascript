e006d98db7364983aeddfc8f35f4e20a1bc5a830
pawanuikey06 <pawanuikey690@gmail.com>
2024-11-06 12:28:19 +0530

    yo

---
+++ EVENTS/script.js
@@ -0,0 +1,16 @@
+// document.getElementById('owl').onclick =()=>{
+//     alert('OWL CLICKED')
+// }
+
+
+document.getElementById('owl').addEventListener('click',(event)=>{
+
+    console.log(event)
+
+},false)
+//  HOMEWORK
+// TYPE of Event Listener, Timsestamp ,defaultPrevented ,
+// Target ,toElement ,srcElement , currentTarget 
+// ClientX,ClientY ,screenX,screenY ,altKEy ,ctrlKey,shiftkey,keyCode
+
+// EVENT PROPOGATION
