843f20cc1b8e799d9d5f58a4400231a7d4e8e8f0
pawanuikey06 <pawanuikey690@gmail.com>
2024-11-14 09:47:58 +0530

    opp

--- OOPs/OOPs.js
+++ OOPs/OOPs.js
@@ -1,13 +1,2 @@
 // object literal or base unit js
 
-const user ={
-    username : "pawan",
-    logincount :8,
-    signedIn :true,
-    getuserDetails : function(){
-        console.log("Got user details from database");
-    }
-}
-
-console.log(user.username);
-console.log(user.getuserDetails());
