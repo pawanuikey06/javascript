74d4138261398f09224c22a3ee20b0b25fde1601
Pawan Uikey <pawanuikey690@gmail.com>
2024-12-30 19:28:00 +0530

    Promise Methods

---
+++ Promises/PromiseAll.js
@@ -0,0 +1,23 @@
+
+Promise.all([
+    new Promise(resolve => setTimeout(() => resolve(1), 3000)), // 1
+    new Promise(resolve => setTimeout(() => resolve(2), 2000)), // 2
+    new Promise(resolve => setTimeout(() => resolve(3), 1000))  // 3
+  ]).then(value=>console.log(value)); 
+
+
+
+  let urls = [
+    'https://api.github.com/users/iliakan',
+    'https://api.github.com/users/remy',
+    'https://api.github.com/users/jeresig'
+  ];
+  
+  // map every url to the promise of the fetch
+  let requests = urls.map(url => fetch(url));
+  
+  // Promise.all waits until all jobs are resolved
+  Promise.all(requests)
+    .then(responses => responses.forEach(
+      response => console.log(`${response.url}: ${response.status}`)
+    ));
