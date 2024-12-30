437e079f9b86beb9d61e9a3f3dbffb5b2c410451
Pawan Uikey <pawanuikey690@gmail.com>
2024-12-30 18:26:40 +0530

    Promises

--- Promises/promise.js
+++ Promises/promise.js
@@ -1,39 +1,72 @@
 
-function walkDog(callback){
-    
-    
-    setTimeout(()=>{
-        console.log("you walk the dog")
-        callback();
-    },1500);
-}
+function walkDog(){
 
-
-function cleanKitchen(callback){
-    
-
-    setTimeout(()=>{
-        console.log("You clean the kitchen ");
-        callback();
+    return new Promise((resolve,reject)=>{
         
-    },2500)
-}
+        setTimeout(()=>{
 
+        const dogWalked=true;
+        if(dogWalked){
 
-function takeoutTrash(callback){
-
-    setTimeout(() => {
-
-        console.log("yOU TAKE out the trash")
-        callback();
-        
-    }, 500);
-}
-
-walkDog(()=>{
-    cleanKitchen(()=>{
-        takeoutTrash(()=>{
-            console.log("You Finished all the Chores!")
-        })
+            resolve("DOG WALKED")
+        }else{
+            reject("Dog Didn't Walked");
+        }
+            
+            
+        },1500);
     })
-});
+}
+
+
+function cleanKitchen(){
+    
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+
+            const kitcheCleand =false;
+            if(kitcheCleand){
+                resolve("You clean the kitchen ");
+            }else{
+                reject("Kitchen is not Cleaned")
+            }
+           
+            
+        },2500)
+    })
+}
+
+
+function takeoutTrash(){
+
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+
+            const TrashTakedOut=true;
+            if(TrashTakedOut){
+                resolve("yOU TAKE out the trash");
+            }else{
+                reject("Trash is not Taked OUT !")
+            }
+            
+            
+        }, 500);
+    })
+}
+
+// walkDog(()=>{
+//     cleanKitchen(()=>{
+//         takeoutTrash(()=>{
+//             console.log("You Finished all the Chores!")
+//         })
+//     })
+// });
+
+// Method cahnining
+
+
+walkDog().then(value=>{console.log(value);return cleanKitchen()})
+.then(value=>{console.log(value);return takeoutTrash()})
+.then(value=>{console.log(value);
+    console.log("You Finished All The Chores")}).catch(error=>console.error(error));
+    
