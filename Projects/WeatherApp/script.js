577d1dce339dfe8b9457a75c9da07fabac0cf21b
Pawan Uikey <pawanuikey690@gmail.com>
2025-01-05 22:49:41 +0530

    1st project

--- Projects/WeatherApp/script.js
+++ Projects/WeatherApp/script.js
@@ -1,6 +1,6 @@
-const apiKey ="8e2b0874b20879611977fcd3cd381247";
+const apiKey ="create ur own api key";
 const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&";
-
+const weatherIcon=document.querySelector(".weather-icon")
 async function checkWeather() {
     const city =document.querySelector("#enter").value;
     if(!city){
@@ -11,12 +11,28 @@ async function checkWeather() {
 
     console.log(data);
     document.querySelector(".city").innerHTML=data.name;
-    document.querySelector(".temp").innerHTML=data.main.temp;
+    document.querySelector(".temp").innerHTML=Math.round(data.main.temp)+'°C';
     document.querySelector(".humidity").innerHTML=data.main.humidity+' %  ';
     document.querySelector(".Wind").innerHTML=data.wind.speed+' km/h  ';
 
+    if(data.weather[0].main =="Clouds"){
+        weatherIcon.src="images/clouds.png"
+    }
+    if(data.weather[0].main =="Clear"){
+        weatherIcon.src="images/clear.png"
+    }
+    if(data.weather[0].main =="Rain"){
+        weatherIcon.src="images/rain.png"
+    }
+    if(data.weather[0].main =="Drizzle"){
+        weatherIcon.src="images/drizzle.png"
+
+    }
+    if(data.weather[0].main =="Mist"){
+        weatherIcon.src="images/mist.png"
+    }
     
 
-    
+    document.querySelector(".weather").style.display="block";
 }
 document.getElementById("submit").addEventListener("click",checkWeather)
